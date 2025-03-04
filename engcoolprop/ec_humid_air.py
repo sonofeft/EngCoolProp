c6ad08a3c3ffb7610fa78ce5b538dba0b372f224
Charlie Taylor <sonofeft@users.noreply.github.com>
2025-03-04 15:01:22 -0800

    first version of humid air

--- engcoolprop/ec_humid_air.py
+++ engcoolprop/ec_humid_air.py
@@ -3,9 +3,210 @@ Humid Air Properties
 http://www.coolprop.org/fluid_properties/HumidAir.html#table-of-inputs-outputs-to-hapropssi
 
 """
+import CoolProp.CoolProp as CP
+from engcoolprop.humid_air_params import (preferred_list, param_synonymD, param_eng_unitsD, 
+                                          param_descD, param_si_unitsD)
+
+from engcoolprop.ec_fluid import (CPeng_fromSI ,  CondEng_fromSI ,   DSI_fromEng ,  CPSI_fromEng,
+                                  Deng_fromSI ,   PSI_fromEng, Peng_fromSI, EchoInput,
+                                  SSI_fromEng ,  Seng_fromSI ,  TSI_fromEng ,  
+                                  Teng_fromSI ,  UHSI_fromEng ,  UHeng_fromSI ,   Veng_fromSI,
+                                  CondSI_fromEng, VSI_fromEng  )
+
+
+def Voleng_fromSI( Vol ): # 1 lbm/ft^3 = 16.01843417 kg/m^3
+    return Vol * 16.01843417
+
+def VolSI_fromEng( Vol ): # 1 lbm/ft^3 = 16.01843417 kg/m^3
+    return Vol / 16.01843417
+
+
+# map Eng to SI conversion functions for each of the fluid properties
+toSI_callD = {} # index=eng, value=conversion func (e.g. SSI_fromEng)
+toSI_callD['Conductivity'] = CondSI_fromEng
+toSI_callD['cp'] = CPSI_fromEng
+toSI_callD['cp_ha'] = CPSI_fromEng
+toSI_callD['CV'] = CPSI_fromEng
+toSI_callD['CVha'] = CPSI_fromEng
+toSI_callD['DewPoint'] = TSI_fromEng
+toSI_callD['Hda'] = UHSI_fromEng
+toSI_callD['Hha'] = UHSI_fromEng
+toSI_callD['HumRat'] = EchoInput
+toSI_callD['P'] = PSI_fromEng
+toSI_callD['P_w'] = PSI_fromEng
+toSI_callD['RelHum'] = EchoInput
+toSI_callD['Sda'] = SSI_fromEng
+toSI_callD['Sha'] = SSI_fromEng
+toSI_callD['Tdb'] = TSI_fromEng
+toSI_callD['Vda'] = VolSI_fromEng
+toSI_callD['Vha'] = VolSI_fromEng
+toSI_callD['Visc'] = VSI_fromEng
+toSI_callD['WetBulb'] = TSI_fromEng
+toSI_callD['Y'] = EchoInput
+toSI_callD['Z'] = EchoInput
+
+# need to add to toEng_callD with all synonyms for preferred names
+for pref_name in preferred_list:
+    syn_set = param_synonymD[pref_name]
+    for syn in syn_set:
+        if syn not in toSI_callD:
+            toSI_callD[syn] = toSI_callD[pref_name]
+
+
+# ==========================================================
+# map SI to Eng conversion functions for each of the fluid properties
+toEng_callD = {} # index=si char, value=conversion func (e.g. Seng_fromSI)
+
+toEng_callD['Conductivity'] = CondEng_fromSI
+toEng_callD['cp'] = CPeng_fromSI
+toEng_callD['cp_ha'] = CPeng_fromSI
+toEng_callD['CV'] = CPeng_fromSI
+toEng_callD['CVha'] = CPeng_fromSI
+toEng_callD['DewPoint'] = Teng_fromSI
+toEng_callD['Hda'] = UHeng_fromSI
+toEng_callD['Hha'] = UHeng_fromSI
+toEng_callD['HumRat'] = EchoInput
+toEng_callD['P'] = Peng_fromSI
+toEng_callD['P_w'] = Peng_fromSI
+toEng_callD['RelHum'] = EchoInput
+toEng_callD['Sda'] = Seng_fromSI
+toEng_callD['Sha'] = Seng_fromSI
+toEng_callD['Tdb'] = Teng_fromSI
+toEng_callD['Vda'] = Voleng_fromSI
+toEng_callD['Vha'] = Voleng_fromSI
+toEng_callD['Visc'] = Veng_fromSI
+toEng_callD['WetBulb'] = Teng_fromSI
+toEng_callD['Y'] = EchoInput
+toEng_callD['Z'] = EchoInput
 
 
 class EC_Humid_Air(object):
     
-    def __init__(self, T=None ,P=None):
-        pass
+    def __init__(self,  **kwargs):
+
+        self.si_propD = {} # key:preferred name, value: value in SI units
+        self.eng_propD = {} # key:preferred name, value: value in Engineering units
+        self.setProps( **kwargs )
+
+    def setProps(self, **kwargs):
+        """
+        Calculates various properties of humid air using CoolProp's HAPropsSI function.
+
+        Args:
+        - kwargs (dict): Dictionary with three keys corresponding to the input properties and their values.
+                         The values in kwargs are in Engineering units
+
+        Returns:
+        - dict: A dictionary with property names as keys and their calculated values as values.
+        """
+        # Allow user to call setProps with none or partial parameters
+        if len(kwargs) == 0:
+            kwargs = {'T':536.4 ,'P':14.6959, 'RelHum':0.5}
+        elif len(kwargs) == 2 and 'P' not in kwargs:
+            kwargs['P'] = 14.6959
+
+        # Ensure the kwargs dictionary has exactly three items
+        if len(kwargs) != 3:
+            raise ValueError("Exactly three input properties must be provided")
+        
+        if 'P' not in kwargs:
+            raise ValueError('"P" must be specified')
+        
+        self.eng_inputD = kwargs.copy()
+
+        self.si_inputD = {}
+        for k,v in kwargs.items():
+            self.si_inputD[k] = toSI_callD[k](v)
+
+
+        # Extract keys and values from the self.si_inputD dictionary
+        input_keys = list(self.si_inputD.keys())
+        input_vals = list(self.si_inputD.values())
+
+        # Iterate through the list of properties and calculate each one
+        for prop in preferred_list:
+            try:
+                # Place SI results into self.si_propD
+                self.si_propD[prop] = CP.HAPropsSI(prop, input_keys[0], input_vals[0], input_keys[1], input_vals[1], input_keys[2], input_vals[2])
+            except:
+                print( 'Failed to calc:', prop)
+                self.si_propD[prop] = float('inf')
+                
+
+        # convert properties from SI to Engineering
+        for k,v in self.si_propD.items():
+            self.eng_propD[k] = toEng_callD[k](v)
+
+        # include shorter, non-CoolProp name for Conductivity
+        self.si_propD['Cond'] = self.si_propD['Conductivity']
+        self.eng_propD['Cond'] = self.eng_propD['Conductivity']
+
+        self.eng_propD['Visc'] *= 1.0E5
+
+        # if 'Y' in self.eng_propD and self.eng_propD['Y'] < 1.0:
+        #     # Constants
+        #     M_dry_air = 28.96  # Molecular weight of dry air in g/mol
+        #     M_water_vapor = 18.0153  # Molecular weight of water vapor in g/mol
+        #     Y = self.eng_propD['Y']
+
+        #     MolWt = (1 - Y) * M_dry_air + Y * M_water_vapor
+        #     self.eng_propD['MolWt'] = MolWt
+        #     self.si_propD['MolWt'] = MolWt
+
+    def get_eng_fmt_size(self):
+        """Make a g format sized for largest number of property in propL"""
+        # sL = ['%g'%self.eng_propD[name] for name in propL]
+        max_len = max([len('%g'%self.eng_propD[name]) for name in preferred_list])
+        return '%' + '%ig'%max_len
+
+    def get_si_fmt_size(self):
+        """Make a g format sized for largest number of property in propL"""
+        # sL = ['%g'%self.eng_propD[name] for name in propL]
+        max_len = max([len('%g'%self.si_propD[name]) for name in preferred_list])
+        return '%' + '%ig'%max_len
+
+    def printProps(self, eng_units=True):
+        '''print a multiline property summary with units'''
+
+        
+        def dict_to_string(D):
+            return ', '.join([f"{key}={value:g}" for key, value in D.items()])
+
+
+        print_orderL = ["Tdb","DewPoint","WetBulb","P","P_w","",
+        "Vda","Vha","","cp","cp_ha","CV","CVha","","Hda","Hha","Sda","Sha",
+        "",     "Visc","Cond","","RelHum","HumRat","Y","Z"]
+
+        if eng_units:
+            s = dict_to_string( self.eng_inputD )
+            fmt = self.get_eng_fmt_size( )
+            propD = self.eng_propD
+            unitsD = param_eng_unitsD
+        else:
+            s = dict_to_string( self.si_inputD )
+            fmt = self.get_si_fmt_size( )
+            propD = self.si_propD
+            unitsD = param_si_unitsD
+        print("============ State Point for Humid Air (%s) ============"%s)
+
+        for name in print_orderL:
+            if name:
+                print("%10s ="%name, fmt%propD[name], unitsD[name],
+                      " :: %s"%param_descD[name])
+            else:
+                print()
+
+
+if __name__ == "__main__":
+
+    ha = EC_Humid_Air( T=536.4 , RelHum=0.5 )
+
+    kL = sorted( ha.eng_propD.keys(), key=str.lower)
+    for k in kL:
+        print( '%10s = %12g'%(k,ha.eng_propD[k]), '= %12G'%ha.si_propD[k])
+
+    print()
+
+    ha.printProps()
+    print('.'*66)
+    ha.printProps(eng_units=False)
---
+++ engcoolprop/humid_air_params.py
@@ -0,0 +1,274 @@
+
+input_set = set() # set of input params
+
+output_set = set() # set of output params
+
+param_si_unitsD = {} # key:name, value:SI units
+param_eng_unitsD= {} # key:name, value:Engineering units
+
+param_descD = {} # key:name, value:description
+param_synonymD = {} # key:name, value: set of all synonyms
+
+preferred_nameD = {} # key:preferred name, value:description
+
+
+# ============= input_set ===================
+input_set = {'DewPoint', 'Sha', 'T_dp', 'H', 'P', 'W', 'psi_w', 'R', 'Tdb', 'Entropy', 'RelHum', 
+             'WetBulb', 'Enthalpy', 'T_db', 'Hda', 'D', 'T', 'V', 'B', 'Y', 'Vha', 'Omega', 'Sda', 
+             'S', 'Hha', 'HumRat', 'Twb', 'P_w', 'RH', 'Vda', 'T_wb', 'Tdp'}
+
+input_list = sorted( input_set, key=str.lower)
+
+# ============= output_set ===================
+output_set = {'DewPoint', 'Visc', 'mu', 'Sha', 'Z', 'T_dp', 'M', 'cv_ha', 'H', 'P', 'W', 'psi_w', 
+              'CV', 'R', 'Tdb', 'Entropy', 'RelHum', 'CVha', 'WetBulb', 'Enthalpy', 'k', 'T_db', 
+              'Hda', 'D', 'cp', 'T', 'V', 'B', 'Y', 'Vha', 'Omega', 'Sda', 'S', 'Hha', 'HumRat', 
+              'Twb', 'Conductivity', 'P_w', 'RH', 'Cha', 'C', 'Vda', 'T_wb', 'cp_ha', 'Tdp', 'K'}
+
+output_list = sorted( output_set, key=str.lower)
+
+# ============= param_si_unitsD ===================
+param_si_unitsD["B"] = "K"
+param_si_unitsD["Twb"] = "K"
+param_si_unitsD["T_wb"] = "K"
+param_si_unitsD["WetBulb"] = "K"
+param_si_unitsD["C"] = "J/kg dry air/K"
+param_si_unitsD["cp"] = "J/kg dry air/K"
+param_si_unitsD["Cha"] = "J/kg humid air/K"
+param_si_unitsD["cp_ha"] = "J/kg humid air/K"
+param_si_unitsD["CV"] = "J/kg dry air/K"
+param_si_unitsD["CVha"] = "J/kg humid air/K"
+param_si_unitsD["cv_ha"] = "J/kg humid air/K"
+param_si_unitsD["D"] = "K"
+param_si_unitsD["Tdp"] = "K"
+param_si_unitsD["DewPoint"] = "K"
+param_si_unitsD["T_dp"] = "K"
+param_si_unitsD["H"] = "J/kg dry air"
+param_si_unitsD["Hda"] = "J/kg dry air"
+param_si_unitsD["Enthalpy"] = "J/kg dry air"
+param_si_unitsD["Hha"] = "J/kg humid air"
+param_si_unitsD["K"] = "W/m/K"
+param_si_unitsD["k"] = "W/m/K"
+param_si_unitsD["Conductivity"] = "W/m/K"
+param_si_unitsD["M"] = "Pa-s"
+param_si_unitsD["Visc"] = "Pa-s"
+param_si_unitsD["mu"] = "Pa-s"
+param_si_unitsD["psi_w"] = "mol water/mol humid air"
+param_si_unitsD["Y"] = "mol water/mol humid air"
+param_si_unitsD["P"] = "Pa"
+param_si_unitsD["P_w"] = "Pa"
+param_si_unitsD["R"] = ""
+param_si_unitsD["RH"] = ""
+param_si_unitsD["RelHum"] = ""
+param_si_unitsD["S"] = "J/kg dry air/K"
+param_si_unitsD["Sda"] = "J/kg dry air/K"
+param_si_unitsD["Entropy"] = "J/kg dry air/K"
+param_si_unitsD["Sha"] = "J/kg humid air/K"
+param_si_unitsD["T"] = "K"
+param_si_unitsD["Tdb"] = "K"
+param_si_unitsD["T_db"] = "K"
+param_si_unitsD["V"] = "m^3 /kg dry air"
+param_si_unitsD["Vda"] = "m^3 /kg dry air"
+param_si_unitsD["Vha"] = "m^3 /kg humid air"
+param_si_unitsD["W"] = "kg water/kg dry air"
+param_si_unitsD["Omega"] = "kg water/kg dry air"
+param_si_unitsD["HumRat"] = "kg water/kg dry air"
+param_si_unitsD["Z"] = ""
+
+# Non-CoolProp names
+param_si_unitsD["MolWt"] = "g/gmole"
+param_si_unitsD["Cond"] = "W/m/K"
+
+# ============= param_eng_unitsD ===================
+param_eng_unitsD["B"] = "degR"
+param_eng_unitsD["Twb"] = "degR"
+param_eng_unitsD["T_wb"] = "degR"
+param_eng_unitsD["WetBulb"] = "degR"
+param_eng_unitsD["C"] = "BTU/lbm dry air/degR"  
+param_eng_unitsD["cp"] = "BTU/lbm dry air/degR"
+param_eng_unitsD["Cha"] = "BTU/lbm humid air/degR"
+param_eng_unitsD["cp_ha"] = "BTU/lbm humid air/degR"
+param_eng_unitsD["CV"] = "BTU/lbm dry air/degR"
+param_eng_unitsD["CVha"] = "BTU/lbm humid air/degR"
+param_eng_unitsD["cv_ha"] = "BTU/lbm humid air/degR"
+param_eng_unitsD["D"] = "degR"
+param_eng_unitsD["Tdp"] = "degR"
+param_eng_unitsD["DewPoint"] = "degR"
+param_eng_unitsD["T_dp"] = "degR"
+param_eng_unitsD["H"] = "BTU/lbm dry air"  
+param_eng_unitsD["Hda"] = "BTU/lbm dry air"
+param_eng_unitsD["Enthalpy"] = "BTU/lbm dry air"
+param_eng_unitsD["Hha"] = "BTU/lbm humid air"
+param_eng_unitsD["K"] = "BTU/ft-hr-R"
+param_eng_unitsD["k"] = "BTU/ft-hr-R"
+param_eng_unitsD["Conductivity"] = "BTU/ft-hr-R"
+param_eng_unitsD["M"] = "[1.0E5 * lbm/ft-sec]"
+param_eng_unitsD["Visc"] = "[1.0E5 * lbm/ft-sec]"
+param_eng_unitsD["mu"] = "[1.0E5 * lbm/ft-sec]"
+param_eng_unitsD["psi_w"] = "mol water/mol humid air"
+param_eng_unitsD["Y"] = "mol water/mol humid air"
+param_eng_unitsD["P"] = "psia"
+param_eng_unitsD["P_w"] = "psia"
+param_eng_unitsD["R"] = ""
+param_eng_unitsD["RH"] = ""
+param_eng_unitsD["RelHum"] = ""
+param_eng_unitsD["S"] = "BTU/lbm dry air/degR"
+param_eng_unitsD["Sda"] = "BTU/lbm dry air/degR"
+param_eng_unitsD["Entropy"] = "BTU/lbm dry air/degR"
+param_eng_unitsD["Sha"] = "BTU/lbm humid air/degR"
+param_eng_unitsD["T"] = "degR"
+param_eng_unitsD["Tdb"] = "degR"
+param_eng_unitsD["T_db"] = "degR"
+param_eng_unitsD["V"] = "ft^3/lbm dry air"
+param_eng_unitsD["Vda"] = "ft^3/lbm dry air"
+param_eng_unitsD["Vha"] = "ft^3/lbm humid air"
+param_eng_unitsD["W"] = "lbm water/lbm dry air"
+param_eng_unitsD["Omega"] = "lbm water/lbm dry air"
+param_eng_unitsD["HumRat"] = "lbm water/lbm dry air"
+param_eng_unitsD["Z"] = ""
+
+# Non-CoolProp names
+param_eng_unitsD["MolWt"] = "lbm/lbmmole"
+param_eng_unitsD["Cond"] = "BTU/ft-hr-R"
+
+
+# ============= param_descD ===================
+param_descD["B"] = "Wet-Bulb Temperature"
+param_descD["Twb"] = "Wet-Bulb Temperature"
+param_descD["T_wb"] = "Wet-Bulb Temperature"
+param_descD["WetBulb"] = "Wet-Bulb Temperature"
+param_descD["C"] = "Mixture Cp per unit dry air"
+param_descD["cp"] = "Mixture Cp per unit dry air"
+param_descD["Cha"] = "Mixture Cp per unit humid air"
+param_descD["cp_ha"] = "Mixture Cp per unit humid air"
+param_descD["CV"] = "Mixture Cv per unit dry air"
+param_descD["CVha"] = "Mixture Cv per unit humid air"
+param_descD["cv_ha"] = "Mixture Cv per unit humid air"
+param_descD["D"] = "Dew-Point Temperature"
+param_descD["Tdp"] = "Dew-Point Temperature"
+param_descD["DewPoint"] = "Dew-Point Temperature"
+param_descD["T_dp"] = "Dew-Point Temperature"
+param_descD["H"] = "Mixture enthalpy per dry air"
+param_descD["Hda"] = "Mixture enthalpy per dry air"
+param_descD["Enthalpy"] = "Mixture enthalpy per dry air"
+param_descD["Hha"] = "Mixture enthalpy per humid air"
+param_descD["K"] = "Mixture thermal conductivity"
+param_descD["k"] = "Mixture thermal conductivity"
+param_descD["Conductivity"] = "Mixture thermal conductivity"
+param_descD["M"] = "Mixture viscosity"
+param_descD["Visc"] = "Mixture viscosity"
+param_descD["mu"] = "Mixture viscosity"
+param_descD["psi_w"] = "Water mole fraction"
+param_descD["Y"] = "Water mole fraction"
+param_descD["P"] = "Pressure"
+param_descD["P_w"] = "Partial pressure of water vapor"
+param_descD["R"] = "Relative humidity in range [0, 1]"
+param_descD["RH"] = "Relative humidity in range [0, 1]"
+param_descD["RelHum"] = "Relative humidity in range [0, 1]"
+param_descD["S"] = "Mixture entropy per unit dry air"
+param_descD["Sda"] = "Mixture entropy per unit dry air"
+param_descD["Entropy"] = "Mixture entropy per unit dry air"
+param_descD["Sha"] = "Mixture entropy per unit humid air"
+param_descD["T"] = "Dry-Bulb Temperature"
+param_descD["Tdb"] = "Dry-Bulb Temperature"
+param_descD["T_db"] = "Dry-Bulb Temperature"
+param_descD["V"] = "Mixture volume per unit dry air"
+param_descD["Vda"] = "Mixture volume per unit dry air"
+param_descD["Vha"] = "Mixture volume per unit humid air"
+param_descD["W"] = "Humidity Ratio"
+param_descD["Omega"] = "Humidity Ratio"
+param_descD["HumRat"] = "Humidity Ratio"
+param_descD["Z"] = "Compressibility factor (Z=pv/(RT))"
+
+# Non-CoolProp names
+param_descD["MolWt"] = "Mixture Molecular Weight"
+param_descD["Cond"] = "Mixture thermal conductivity"
+
+# ============= param_synonymD ===================
+param_synonymD["B"] = "{'Twb', 'B', 'T_wb', 'WetBulb'}"
+param_synonymD["Twb"] = "{'Twb', 'B', 'T_wb', 'WetBulb'}"
+param_synonymD["T_wb"] = "{'Twb', 'B', 'T_wb', 'WetBulb'}"
+param_synonymD["WetBulb"] = "{'Twb', 'B', 'T_wb', 'WetBulb'}"
+param_synonymD["C"] = "{'C', 'cp'}"
+param_synonymD["cp"] = "{'C', 'cp'}"
+param_synonymD["Cha"] = "{'cp_ha', 'Cha'}"
+param_synonymD["cp_ha"] = "{'cp_ha', 'Cha'}"
+param_synonymD["CV"] = "{'CV'}"
+param_synonymD["CVha"] = "{'CVha', 'cv_ha'}"
+param_synonymD["cv_ha"] = "{'CVha', 'cv_ha'}"
+param_synonymD["D"] = "{'Tdp', 'T_dp', 'D', 'DewPoint'}"
+param_synonymD["Tdp"] = "{'Tdp', 'T_dp', 'D', 'DewPoint'}"
+param_synonymD["DewPoint"] = "{'Tdp', 'T_dp', 'D', 'DewPoint'}"
+param_synonymD["T_dp"] = "{'Tdp', 'T_dp', 'D', 'DewPoint'}"
+param_synonymD["H"] = "{'Hda', 'Enthalpy', 'H'}"
+param_synonymD["Hda"] = "{'Hda', 'Enthalpy', 'H'}"
+param_synonymD["Enthalpy"] = "{'Hda', 'Enthalpy', 'H'}"
+param_synonymD["Hha"] = "{'Hha'}"
+param_synonymD["K"] = "{'Conductivity', 'k', 'K'}"
+param_synonymD["k"] = "{'Conductivity', 'k', 'K'}"
+param_synonymD["Conductivity"] = "{'Conductivity', 'k', 'K'}"
+param_synonymD["M"] = "{'mu', 'M', 'Visc'}"
+param_synonymD["Visc"] = "{'mu', 'M', 'Visc'}"
+param_synonymD["mu"] = "{'mu', 'M', 'Visc'}"
+param_synonymD["psi_w"] = "{'Y', 'psi_w'}"
+param_synonymD["Y"] = "{'Y', 'psi_w'}"
+param_synonymD["P"] = "{'P'}"
+param_synonymD["P_w"] = "{'P_w'}"
+param_synonymD["R"] = "{'RH', 'R', 'RelHum'}"
+param_synonymD["RH"] = "{'RH', 'R', 'RelHum'}"
+param_synonymD["RelHum"] = "{'RH', 'R', 'RelHum'}"
+param_synonymD["S"] = "{'Sda', 'Entropy', 'S'}"
+param_synonymD["Sda"] = "{'Sda', 'Entropy', 'S'}"
+param_synonymD["Entropy"] = "{'Sda', 'Entropy', 'S'}"
+param_synonymD["Sha"] = "{'Sha'}"
+param_synonymD["T"] = "{'T_db', 'Tdb', 'T'}"
+param_synonymD["Tdb"] = "{'T_db', 'Tdb', 'T'}"
+param_synonymD["T_db"] = "{'T_db', 'Tdb', 'T'}"
+param_synonymD["V"] = "{'Vda', 'V'}"
+param_synonymD["Vda"] = "{'Vda', 'V'}"
+param_synonymD["Vha"] = "{'Vha'}"
+param_synonymD["W"] = "{'W', 'Omega', 'HumRat'}"
+param_synonymD["Omega"] = "{'W', 'Omega', 'HumRat'}"
+param_synonymD["HumRat"] = "{'W', 'Omega', 'HumRat'}"
+param_synonymD["Z"] = "{'Z'}"
+
+# ============= preferred_nameD ===================
+preferred_nameD["WetBulb"] = "Wet-Bulb Temperature"
+preferred_nameD["cp"]    = "Mixture Cp per unit dry air"
+preferred_nameD["cp_ha"] = "Mixture Cp per unit humid air"
+preferred_nameD["CV"]    = "Mixture Cv per unit dry air"
+preferred_nameD["CVha"]  = "Mixture Cv per unit humid air"
+preferred_nameD["DewPoint"] = "Dew-Point Temperature"
+preferred_nameD["Hda"] = "Mixture enthalpy per dry air"
+preferred_nameD["Hha"] = "Mixture enthalpy per humid air"
+preferred_nameD["Conductivity"] = "Mixture thermal conductivity"
+preferred_nameD["Visc"] = "Mixture viscosity"
+preferred_nameD["Y"] = "Water mole fraction"
+preferred_nameD["P"] = "Pressure"
+preferred_nameD["P_w"] = "Partial pressure of water vapor"
+preferred_nameD["RelHum"] = "Relative humidity in range [0, 1]"
+preferred_nameD["Sda"] = "Mixture entropy per unit dry air"
+preferred_nameD["Sha"] = "Mixture entropy per unit humid air"
+preferred_nameD["Tdb"] = "Dry-Bulb Temperature"
+preferred_nameD["Vda"] = "Mixture volume per unit dry air"
+preferred_nameD["Vha"] = "Mixture volume per unit humid air"
+preferred_nameD["HumRat"] = "Humidity Ratio"
+preferred_nameD["Z"] = "Compressibility factor (Z=pv/(RT))"
+
+preferred_list = sorted( [k for k in preferred_nameD.keys()], key=str.lower)
+preferred_set  = set( preferred_list )
+
+if __name__ == "__main__":
+
+    # make sure that all the descriptions are covered by preferred names
+    desc_set = set()
+    for v in param_descD.values():
+        desc_set.add( v )
+
+    pref_desc_set = set()
+    for v in preferred_nameD.values():
+        pref_desc_set.add( v )
+
+    print( 'len(desc_set)      =', len(desc_set))
+    print( 'len(pref_desc_set) =', len(pref_desc_set))
+
